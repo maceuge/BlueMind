d613515aa04ff9bd26af29e07c8e0ee3ecf78475
maceuge <maceuge@icloud.com>
2016-12-25 23:58:39 -0300

    Ajustes Maqueteado Parte3

--- js/navanim.js
+++ js/navanim.js
@@ -1,26 +1,18 @@
 
-// $(document).ready(function () {
-//   $(window).scroll(function(){
-//     if (window.pageYOffset >= 100) {
-//         $('.navbar').addClass("bluelight-grad");
-//         $('.navbar').css("box-shadow","0px 1px 8px #555555");
-//     } else {
-//         $('.navbar').removeClass("bluelight-grad");
-//         $('.navbar').css("box-shadow","0px 0px 0px");
-//     }
-//   });
-// });
-
-
 $(document).ready(function () {
+    var docpos = $(document).scrollTop();
+
+    if (docpos >= 100) {
+        $('.nav-background').show();
+        $('.navbar').css("box-shadow", "0px 1px 8px #555555");
+    }
+
     $(window).scroll(function(){
         if (window.pageYOffset >= 100) {
-            $('.navbar').css("background", "linear-gradient(to right, #0fb8ad 0%, #1fc8db 51%, #039be5 75%)");
-            $('.navbar').css("transition", "all 1s ease-in-out");
+            $('.nav-background').fadeIn(400);
             $('.navbar').css("box-shadow", "0px 1px 8px #555555");
         } else {
-            $('.navbar').css("background", "transparent");
-            $('.navbar').css("transition", "all 1s ease-in-out");
+            $('.nav-background').fadeOut(400);
             $('.navbar').css("box-shadow", "0px 0px 0px");
         }
     });
